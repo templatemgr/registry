4095016de0d481e8ce11efa8c74c1e64866c7f04
casjay <git-admin@casjaysdev.pro>
2023-08-28 14:08:22 -0400

    🗃️ Committing everything that changed 🗃️
    install.sh

--- install.sh
+++ install.sh
@@ -25,6 +25,8 @@
 # shellcheck disable=SC2199
 # shellcheck disable=SC2317
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+BASH_SET_SAVED_OPTIONS=$(set +o) && set +e -x
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # check for command
 __cmd_exists() { command "$1" >/dev/null 2>&1 || return 1; }
 __function_exists() { command -v "$1" 2>&1 | grep -q "is a function" || return 1; }
@@ -36,11 +38,12 @@ INSTALL_SH_EXIT_STATUS=0
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Define Variables
 TEMPLATE_NAME="registry"
-CONFIG_CHECK_FILE=""
+CONFIG_CHECK_FILE="registry.yaml"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 TMP_DIR="/tmp/config-$TEMPLATE_NAME"
 CONFIG_DIR="/usr/local/share/template-files/config/$TEMPLATE_NAME"
 INIT_DIR="/usr/local/etc/docker/init.d"
+OVER_WRITE_INIT="yes"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 GIT_REPO="https://github.com/templatemgr/$TEMPLATE_NAME"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -66,10 +69,12 @@ mkdir -p "/etc/$TEMPLATE_NAME" "$DEFAULT_CONF_DIR" "$INIT_DIR"
 if [ -d "$TMP_DIR/init-scripts" ]; then
   init_scripts="$(ls -A "$TMP_DIR/init-scripts/" | grep '^' || false)"
   if [ -n "$init_scripts" ]; then
+    mkdir -p "$INIT_DIR"
     for init_script in $init_scripts; do
-      if [ ! -f "$INIT_DIR/$init_script" ]; then
+      if [ "$OVER_WRITE_INIT" = "yes" ] || [ ! -f "$INIT_DIR/$init_script" ]; then
         echo "Installing  $INIT_DIR/$init_script"
-        cp -Rf "$TMP_DIR/init-scripts/$init_script" "$INIT_DIR/$init_script" || true
+        cp -Rf "$TMP_DIR/init-scripts/$init_script" "$INIT_DIR/$init_script" &&
+          chmod -Rf 755 "$INIT_DIR/$init_script" || true
       fi
     done
   fi
@@ -79,7 +84,10 @@ fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 [ -d "$TMP_DIR" ] && rm -Rf "$TMP_DIR"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-[ -f "$DEFAULT_CONF_DIR/$CONFIG_CHECK_FILE" ] || INSTALL_SH_EXIT_STATUS=1
+if [ -n "$CONFIG_CHECK_FILE" ] && [ ! -f "$DEFAULT_CONF_DIR/$CONFIG_CHECK_FILE" ]; then
+  echo "Can not find a config file in $DEFAULT_CONF_DIR"
+  INSTALL_SH_EXIT_STATUS=1
+fi
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # End application
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
